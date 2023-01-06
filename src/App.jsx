851104d9cb38f4a265508f947c7b40f2aa66e2f1
liukun <kun.liu@fullspeed.cn>
2023-01-06 16:37:06 +0800

    feat: 根据不同应用进行页面大小重置

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState, useRef } from 'react'
+import { Application, Sprite, Assets } from 'pixi.js';
 import './App.css'
 
 function App() {
-  console.log(PIXI, 'PIXI')
-  const [count, setCount] = useState(0)
-  console.log('打印两次')
-  let containerDom
-  let mouseX = 0, mouseY = 0;
-  const [app, setApp] = useState('')
-  const [width, setWidth] = useState('')
-  const [height, setHeight] = useState('')
-  const [particles, setParticles] = useState([])
+  const [width, setWidth] = useState(window.innerWidth)
+  const [height, setHeight] = useState(window.innerHeight)
+  let appRef = useRef(null)
 
+  const resizeUpdate = (e) => {
+    // 通过事件对象获取浏览器窗口的高度
+    let h = e.target.innerHeight;
+    let w = e.target.innerWidth;
+    // 对应用进行重置
+    appRef.renderer.resize(w, h)
+    setHeight(h);
+    setWidth(w)
+  };
+
+  // 重置窗口大小
   useEffect(() => {
-    setWidth(document.body.clientWidth)
-    setHeight(document.body.clientHeight)
-    console.log(width,'width',document.body.clientWidth)
-    console.log(height,'height',document.body.clientHeight)
-    const _app = new PIXI.Application({
+    // 页面刚加载完成后获取浏览器窗口的大小
+    let h = window.innerHeight;
+    setHeight(h)
+    let w = window.innerWidth;
+    setWidth(w)
+    // 页面变化时获取浏览器窗口的大小 
+    window.addEventListener('resize', resizeUpdate);
+    return () => {
+      // 组件销毁时移除监听事件
+      window.removeEventListener('resize', resizeUpdate);
+    }
+  }, [])
+  useEffect(() => {
+    appRef = new Application({
       width: width,
-      height: height,
-      backgroundColor: 0x000000,
-      resolution: window.devicePixelRatio, // 设备像素比
+      height: height
     });
-    setApp(_app)
-    containerDom = document.querySelector("#container");
-    // containerDom.appendChild(app.view);
+    appRef.renderer.autoResize = true;
+    document.body.appendChild(appRef.view);
+    // 组件销毁时移除app应用
+    return () => {
+      document.body.removeChild(appRef.view);
+    }
   }, [])
   return (
     <div id="container"></div>
